09f5bb8389589ca3ff45ededdf9d19297821f521
William Fox <wfox96@gmail.com>
2016-04-22 15:38:22 -0700

    Updated Interface Java Doc

--- Cipher.java
+++ Cipher.java
@@ -1,17 +1,45 @@
+/**
+ * 
+ * A basic interface for ciphers
+ * @author William Fox
+ */
+
 package caeserCipher;
 
 public interface Cipher {
 	
-	public void setMessage(String message);
 	
+	/**
+	 * Sets the message to be encoded or decoded
+	 * @param message
+	 */
+	public void setMessage(String message);
+
+	/**
+	 * Returns the message that was encoded or decoded
+	 * @return message
+	 */
 	public String getMessage();
 	
+	/**
+	 * Encrypts the message
+	 */
 	public void encrypt();
-	
+	/**
+	 * Decrypts the message
+	 */
 	public void decrypt();
 	
+	/**
+	 * Returns the state of whether or not to preserve the capitalizations
+	 * @return isPreserveCapitals
+	 */
 	public boolean isPreserveCapitals();
 	
+	/**
+	 * Sets the isPreserveCapitals flag
+	 * @param preserveCapitals
+	 */
 	public void setPreserveCapitals(boolean preserveCapitals);
 
 }
